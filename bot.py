b760807e1651364214f21d79960422f5719ff48f
b10509006 <37055364+b10509006@users.noreply.github.com>
2020-07-26 20:25:53 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,13 +11,13 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    print(F'{member} join!')
+    
     channel = bot.get_channel(736886903588257796)
     await channel.send(f'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
-    print(F'{member} leave!')
+    
     channel = bot.get_channel(736886903588257796)
     await channel.send(f'{member} leave!')
 
